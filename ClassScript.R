b082e610d6e279fcfbbe373dbd8174a342480758
ljpollack <leapollack@campus-109-040.ucdavis.edu>
2016-11-01 13:59:03 -0700

    added linear model simulation

--- ClassScript.R
+++ ClassScript.R
@@ -3,4 +3,13 @@
 x <- rnorm(n = 1000, mean = 0, sd = 3)
 hist(x, nclass=20)
 
+###create linear regression
+m <- 3
+b <- -2
+eps <- rnorm(1000,mean=0,sd=50)
+x <-runif(1000, 0, 100)
+hist(x)
 
+y <- m*x + b + eps
+
+plot (x,y, cex = .05)
